b4e88d5e0bebd23bb797389f76bb4799360247c2
simon alfandari <horaciococodrilo@gmail.com>
2026-05-07 11:43:47 -0300

    ahora baja y se reinicia

---
+++ Assets/Scripts/CajaCollisionDetection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaCollisionDetection : MonoBehaviour
+{
+    public CajaMovement cajaMovementScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cajaMovementScript = GetComponent<CajaMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Piso"))
+        {
+            cajaMovementScript.MoveBoxToRandomPosition();
+        }
+    }
+}
--- Assets/Scripts/CajaMovement.cs
+++ Assets/Scripts/CajaMovement.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class CajaMovement : MonoBehaviour
 {
     public float speed = 2f;
+    float initialYValue;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialYValue = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(0,-speed * Time.deltaTime ,0);
+    }
+    public void MoveBoxToRandomPosition()
+    {
+        //este no vale
+        //transform.position.y = initialYValie;
+        //para probar, solamente hacemos q suba
+        Vector3 newPosition = transform.position;
+        newPosition.y = initialYValue;
+        transform.position = newPosition;
     }
 }
